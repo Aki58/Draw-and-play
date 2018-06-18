31067052a00fd6c792677ab044cf9b7a628bfa4d
akash-singh <aki24695@gmail.com>
2018-06-18 12:33:15 +0530

    first commit

---
+++ Assets/scripts/GolemController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolemController : MonoBehaviour {
+
+	// Use this for initialization
+
+	public Animator anim;
+    
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		anim = GetComponent<Animator>();
+	}
+
+	public void GolemActions(string ActionCommands)
+	{
+		ActionCommands = ActionCommands.Trim();
+        switch (ActionCommands)
+		{
+          case "jump":
+		   anim.Play("jump",-1,0f);
+		   break;
+		  case "hit":
+		   anim.Play("hit",-1,0f);
+		   break;
+          case "walk":
+           anim.Play("walk", -1, 0f);
+           break;
+          case "rage":
+           anim.Play("rage", -1, 0f);
+           break;
+          case "land":
+           anim.Play("land", -1, 0f);
+           break;
+          case "damage":
+           anim.Play("damage", -1, 0f);
+           break;
+          default:
+		   anim.Play("idle",-1,0f);
+		   break; 
+
+		}
+        
+
+	}
+}
---
+++ Assets/scripts/speech.cs
@@ -0,0 +1,132 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.IO;
+using System.Collections;
+using System.Reflection;
+using ApiAiSDK;
+using ApiAiSDK.Model;
+using ApiAiSDK.Unity;
+using Newtonsoft.Json;
+using System.Net;
+using System.Collections.Generic;
+public class speech : MonoBehaviour {
+    //public Text inputTextField;
+    private ApiAiUnity apiAiUnity;
+    private AudioSource aud;
+    public AudioClip listeningSound;
+    public GolemController gobj;
+
+    private readonly JsonSerializerSettings jsonSettings = new JsonSerializerSettings
+    {
+        NullValueHandling = NullValueHandling.Ignore,
+    };
+
+    private readonly Queue<Action> ExecuteOnMainThread = new Queue<Action>();
+
+    // Use this for initialization
+    IEnumerator Start()
+    {
+        // check access to the Microphone
+        yield return Application.RequestUserAuthorization(UserAuthorization.Microphone);
+        if (!Application.HasUserAuthorization(UserAuthorization.Microphone))
+        {
+            throw new NotSupportedException("Microphone using not authorized");
+        }
+
+        ServicePointManager.ServerCertificateValidationCallback = (a, b, c, d) =>
+        {
+            return true;
+        };
+
+        const string ACCESS_TOKEN = "97cc793e443842ef9e48c1e75ba021a9";
+
+        var config = new AIConfiguration(ACCESS_TOKEN, SupportedLanguage.English);
+
+        apiAiUnity = new ApiAiUnity();
+        apiAiUnity.Initialize(config);
+
+        apiAiUnity.OnError += HandleOnError;
+        apiAiUnity.OnResult += HandleOnResult;
+    }
+
+    void HandleOnResult(object sender, AIResponseEventArgs e)
+    {
+        RunInMainThread(() => {
+            var aiResponse = e.Response;
+            if (aiResponse != null)
+            {
+                var outText = JsonConvert.SerializeObject(aiResponse, jsonSettings);
+                Speech sp = JsonUtility.FromJson<Speech>(outText);
+                Debug.Log("Result: " + sp.result.action);
+                gobj.GolemActions(sp.result.action);
+
+            }
+            else
+            {
+                Debug.LogError("Response is null");
+            }
+        });
+    }
+
+    void HandleOnError(object sender, AIErrorEventArgs e)
+    {
+        RunInMainThread(() => {
+            Debug.LogException(e.Exception);
+            Debug.Log(e.ToString());
+
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (apiAiUnity != null)
+        {
+            apiAiUnity.Update();
+        }
+
+        // dispatch stuff on main thread
+        while (ExecuteOnMainThread.Count > 0)
+        {
+            ExecuteOnMainThread.Dequeue().Invoke();
+        }
+    }
+
+    private void RunInMainThread(Action action)
+    {
+        ExecuteOnMainThread.Enqueue(action);
+    }
+
+    public void PluginInit()
+    {
+
+    }
+    public void StartNativeRecognition()
+    {
+        try
+        {
+            apiAiUnity.StartNativeRecognition();
+        }
+        catch (Exception ex)
+        {
+            Debug.LogException(ex);
+        }
+    }
+
+}
+
+[System.Serializable]
+public class Speech
+{
+    public String id;
+    public Result result;
+
+}
+[System.Serializable]
+public class Result
+{
+    public String action;
+
+
+}
